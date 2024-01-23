fb4b4b6f3beff3f00717a789ec9997171e4de596
NataliaReiis <nataliareis208@gmail.com>
2024-01-23 20:18:30 -0300

    feat: types in functions

--- build/index.js
+++ build/index.js
@@ -1,24 +1,6 @@
 "use strict";
-function printaObjeto(pessoa) {
-    console.log(pessoa);
+function addNumber(x, y) {
+    return x + y;
 }
-printaObjeto({
-    name: "nat",
-    vulgo: "reis",
-});
-console.log("ola mundo");
-let meuProduto = {
-    name: "natalia",
-    value: 89.99,
-};
-/* Arrays */
-let dados = ["nat", "ola"];
-let dados2 = ["nat", "ola"];
-/* arrays multi types */
-let infos = ["nat", "02"];
-/* Tuplas = respeita a ordem */
-let boleto = ["agua", 8.58, 154514485];
-/* arrays métodos = iguais ao do js */
-/* Datas */
-let aniversario = new Date("2024-23-01 19:55");
-console.log(aniversario.toString());
+let soma = addNumber(4, 5);
+console.log(soma);
---
+++ src/functions.ts
@@ -0,0 +1,19 @@
+function addNumber( x: number, y:number): number {
+    return x + y
+}
+let soma:number = addNumber(4, 5)
+console.log(soma)
+
+///////////////////////////////////////////////////
+
+function CallTophone(phone:number | string) : any {
+    return phone
+}
+let somaa: number = addNumber(4, 7);
+console.log(somaa)
+
+/* Função async */
+
+async function getDatabase(id: number): Promise<string> { 
+    return "aiaiaia"
+}
